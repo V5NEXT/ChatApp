744f5a8b2bf08d6f1051c2efcba05aa199f5d5e3
Vishnu Viswambharan <vviswambharan@DRGT.systems>
2021-08-30 15:32:36 +0200

    First Commit

---
+++ src/db.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const config = {
+    apiKey: "AIzaSyAz3NGWwWjBmqa2nl2zq1zMf0jkz5JeA3M",
+    authDomain: "drgt-chat.firebaseapp.com",
+    projectId: "drgt-chat",
+    storageBucket: "drgt-chat.appspot.com",
+    messagingSenderId: "640602591761",
+    appId: "1:640602591761:web:f5cb76521ac789844f97b0",
+    measurementId: "G-FT2XYWZ65V"
+}
+
+const db = firebase.initializeApp(config);
+export default db;
